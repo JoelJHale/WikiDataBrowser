16c4e7a1efd22287213d9d197c40bc4ca20e8414
s-joel.hale <s-joel.hale@CloudComputingGurus.onmicrosoft.com>
2023-05-20 21:56:01 -0700

    Added global header and finished Setting Page

---
+++ src/Header.js
@@ -0,0 +1,4 @@
+var header = document.createElement("header")
+header.innerHTML = "<a href='index.html' class='headerItem'>Home</a><a href='EditSettings.html' class='headerItem'>Add a Site</a><a href='Sites.html' class='headerItem'>View Sites</a>"
+
+document.body.prepend(header)
--- src/SettingObject.js
+++ src/SettingObject.js
@@ -51,19 +51,62 @@ class Settings{
         if (page.includes("Category:")) option.source_type = "Category"
         else option.source_type = "Page_Links"
     }
+    async SetOptionType(optionID, type)
+    {
+        var option = this.GetOption(optionID)
+        option.source_type = type
+        if (type == "Category" && !option.source_page.includes("Category:")) option.source_page = "Category:" + option.source_page
+
+    }
     RemoveOptionPoint(optionID, point)
     {
         var option = this.GetOption(optionID)
-        
-        
-        if (page.includes("Category:")) option.source_type = "Category"
-        else option.source_type = "Page_Links"
+        if (option) option.DeleteDataPoint(point)   
+    }
+    EditDataPoint(optionID, oldPoint, newData)
+    {
+        console.log(newData)
+        console.log(oldPoint)
+        var option = this.GetOption(optionID)
+        this.RemoveOptionPoint(optionID, oldPoint)
+        var obj = new Data(newData);
+        option.array_data[newData.locationType].push(obj)
+
     }
 }
 class Options{
     source_page
     source_type
-    array_data = {"properties":null, "page_templates":null}
+    array_data = {"properties":[], "page_templates":[]}
+
+    
+    FindDataPoint(id)
+    {
+        for (var i in this.array_data)
+        {
+            if (this.array_data[i] == null) continue
+            var val = this.array_data[i].find((obj) => obj.displayName == id)
+            if (val) return val
+        }
+        return null
+    }
+    DeleteDataPoint(id)
+    {
+        for (var i in this.array_data)
+        {
+            var oldArray = this.array_data[i]
+            if (oldArray == null) continue
+            var newArray = []
+            for (var j in oldArray)
+            {
+                var elem = oldArray[j]
+                console.log(elem)
+                if (elem.displayName != id) newArray.push(elem)
+            }
+            this.array_data[i] = newArray
+        }
+        return
+    }
     
 }
 class Data{
@@ -72,4 +115,18 @@ class Data{
     displayName
     required
     default
+    constructor(data){
+        this.location = data.location
+        this.type = data.type
+        this.displayName = data.displayName
+        this.required = data.required
+        this.default = data.default
+    }
+    Set(data){
+        this.location = data.location
+        this.type = data.type
+        this.displayName = data.displayName
+        this.required = data.required
+        this.default = data.default
+    }
 }
